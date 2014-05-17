7d0d698e11140c092b0adc51b80257b321ee56cb
Josh The Geek <joshthegeek@me.com>
2014-05-16 21:50:41 -0400

    Add workaround that makes completion actually work.

--- bin/grunt-init-completion.js
+++ bin/grunt-init-completion.js
@@ -13,21 +13,16 @@ if (process.argv.indexOf('completion') !== -1 && process.argv.indexOf('--') === 
 	process.argv.push('grunt-init-completion');
 }
 
-tab()
-	('-i', function(callback) {
-		fs.readdir(path.join(homedir), function(err, files) {
-			if (err) callback(err);
+fs.readdir(path.join(homedir), function(err, files) {
+	if (err) process.exit(2);
 
-			files = files.filter(function(file) {
-				return fs.statSync(path.join(homedir, file)).isDirectory();
-			});
+	files = files.filter(function(file) {
+		return fs.statSync(path.join(homedir, file)).isDirectory();
+	});
 
-			if (files.length) return callback(null, files, {
-				exitCode:15,
-			});
+	files.forEach(function(file) {
+		tab(file)(noop);
+	});
 
-			callback();
-		});
-	})
-
-tab.parse();
+	tab.parse();
+});
